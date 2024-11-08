c7bacf7c6c5a63cce60fcc2e35a574b9713c6a94
pctrainor <trainorp14@gmail.com>
2024-11-08 01:38:47 -0600

    added resume section

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const educationSection = document.getElementById('education');
 const experienceSection = document.getElementById('experience');
+const pdfSection = document.getElementById('pdf-section');
 
 function handleSectionClick(section) {
-  const content = section.querySelector('.education-content, .experience-content');
-
-  content.style.display = content.style.display === 'block' ? 'none' : 'block';
-  content.style.fontSize = content.style.fontSize === '1.05em' ? 'initial' : '1.05em'; // Adjust font size as needed
+  const content = section.querySelector('.education-content, .experience-content, .pdf-content');
+  
+  if (content) {
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    content.style.fontSize = content.style.fontSize === '1.05em' ? 'initial' : '1.05em'; // Adjust font size as needed
+  }
 }
 
-educationSection.addEventListener('click', () => handleSectionClick(educationSection));
-experienceSection.addEventListener('click', () => handleSectionClick(experienceSection));
+if (educationSection) {
+  educationSection.addEventListener('click', () => handleSectionClick(educationSection));
+}
+if (experienceSection) {
+  experienceSection.addEventListener('click', () => handleSectionClick(experienceSection));
+}
+if (pdfSection) {
+  pdfSection.addEventListener('click', () => handleSectionClick(pdfSection));
+}
